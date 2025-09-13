02623bd4b985fe8d9c6411063ff6ccf4f4109aa8
akashnale1998 <naleakash173050@gmail.com>
2025-09-13 21:49:25 +0530

    Update redisClient.js

--- CHAT APP BACKEND/redis/redisClient.js
+++ CHAT APP BACKEND/redis/redisClient.js
@@ -1,20 +1,20 @@
 const Redis = require('ioredis');
 
 // Connect to local Redis server
-const redis = new Redis({
-  // host: '127.0.0.1',  // Redis server host
-    host: 'redis',  // Redis server host
+// const redis = new Redis({
+//   // host: '127.0.0.1',  // Redis server host
+//     host: 'redis',  // Redis server host
 
-  port: 6379,         // Redis default port
-  // password: 'yourpassword', // uncomment if Redis has password
-});
+//   port: 6379,         // Redis default port
+//   // password: 'yourpassword', // uncomment if Redis has password
+// });
 
-redis.on('connect', () => {
-  console.log('Connected to Redis');
-});
+// redis.on('connect', () => {
+//   console.log('Connected to Redis');
+// });
 
-redis.on('error', (err) => {
-  console.error('Redis error:', err);
-});
+// redis.on('error', (err) => {
+//   console.error('Redis error:', err);
+// });
 
 module.exports = redis;
